6381a4614ac876370b774a0ef961fc9a2501f03f
Manojbabu <83714923+manusoft@users.noreply.github.com>
2023-06-11 16:01:19 +0400

    Create json_link_parser.py

---
+++ scorpio/v1/json_link_parser.py
@@ -0,0 +1,10 @@
+import json
+
+# Assuming the JSON data is stored in a file named 'data.json'
+with open('iptv.json') as json_file:
+    data = json.load(json_file)
+
+# Accessing the link inside the JSON
+link = data['link']
+
+print(link)  # Output: https://manusoft.github.io/api/scorpio/v1/
